4ddbe7b12dc76f081560aba3db3b2c9cc41c5ed7
PeterHgg <peterli427fh@163.com>
2026-03-05 14:09:04 +0800

    Add missing Vue entry files and config

---
+++ client/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <CsMahjongView />
+</template>
+
+<script setup>
+import CsMahjongView from './games/mahjong/cs_mahjong_view.vue'
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+</style>
